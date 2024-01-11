d62451b802241fb1d4da54ec19e6a9fff4fd90d6
İzzet Özüberk <izzetozuberkreal@gmail.com>
2024-01-12 02:41:16 +0300

    Repository Edit

---
+++ ECommerce.Core/DTO/BaseListDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Core.DTO
+{
+    public class BaseListDTO
+    {
+        public int ID { get; set; }
+        public bool Active { get; set; }
+        public DateTime AddedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public int? UserId { get; set; }
+    }
+}
