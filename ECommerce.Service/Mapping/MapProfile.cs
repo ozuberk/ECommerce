8e7dc1c9ff1afe92f01469174ffe0fa7de2f9c37
İzzet Özüberk <izzetozuberkreal@gmail.com>
2024-01-12 02:32:06 +0300

    Services Edit

---
+++ ECommerce.Service/Mapping/MapProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ECommerce.Core.ECommerceDatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Service.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Products, ProductsDTO>().ReverseMap();
+        }
+    }
+}
