71e736a9c8c3ae93eb72439e6dcb41fdbd902d86
İzzet Özüberk <izzetozuberkreal@gmail.com>
2024-01-25 15:39:45 +0300

    Template Edit

---
+++ ECommerce.Web/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult ProductsIndex()
+        {
+            return View();
+        }
+    }
+}
